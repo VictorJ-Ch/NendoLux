8d2f95bae641548574302bee22b7e07729d28556
Kaseem <chavarria.martinez.victor101@gmail.com>
2024-10-06 22:31:57 -0600

    Items docs ready 2 go
    + all item docs now available

--- README.md
+++ README.md
@@ -22,4 +22,13 @@ NendoLux es una empresa de e-commerce especializada en la venta de Nendoroids de
 - JSONs.
 - Icons.
 - Call of the night docs optimization web / mobile.
+- Chainsaw Man docs optimization web / mobile.
+- Kaguya Sama docs optimization web / mobile.
+- Kamikatsu docs optimization web / mobile.
+- Kimetsu No Yaiba docs optimization web / mobile.
+- Kobayashi's Dragon Maid docs optimization web / mobile.
+- Persona 3 docs optimization web / mobile.
+- Persona 5 docs optimization web / mobile.
+- Persona 5 Royal docs optimization web / mobile.
+- Tomo Chan Is A Girl docs optimization web / mobile.
 - Cart.
--- docs/JS/cart.js
+++ docs/JS/cart.js
@@ -2,42 +2,36 @@ let cart = [];
 
 // Adding product to cart
 function addToCart(product) {
-    // Buscar si el producto ya está en el carrito
     const existingProduct = cart.find(item => item.Name === product.Name);
     if (existingProduct) {
-        // Si ya existe, incrementar la cantidad
         existingProduct.Quantity++;
     } else {
-        // Si no existe, agregarlo con una cantidad de 1
         product.Quantity = 1;
         cart.push(product);
     }
-    updateCartDisplay(); // Actualizar la vista del carrito
+    updateCartDisplay();
 }
 
 // Update cart display
 function updateCartDisplay() {
     const cartItemsContainer = document.getElementById('cart-items');
-    cartItemsContainer.innerHTML = ''; // Limpiar el contenido actual
+    cartItemsContainer.innerHTML = '';
 
     const title = document.createElement('p');
     title.style.fontSize = '20px';
     title.style.textDecoration = 'none';
     title.innerText = 'Agregado a la bolsa de compras';
     cartItemsContainer.appendChild(title);
-    cartItemsContainer.appendChild(document.createElement('hr')); // Añadir separador
+    cartItemsContainer.appendChild(document.createElement('hr'));
 
     if (cart.length === 0) {
-        // Si el carrito está vacío, mostrar mensaje
         const emptyMessage = document.createElement('div');
         emptyMessage.id = 'cart-empty';
         emptyMessage.innerHTML = '<p>Nada aún</p>';
         cartItemsContainer.appendChild(emptyMessage);
     } else {
-        // Si hay productos en el carrito, remover el mensaje vacío
         document.getElementById('cart-empty')?.remove();
 
-        // Mostrar los productos en el carrito
         cart.forEach(item => {
             const li = document.createElement('li');
             li.classList.add('d-flex', 'justify-content-between', 'align-items-center', 'mb-2');
@@ -53,11 +47,12 @@ function updateCartDisplay() {
                     </div>
                 </div>
             `;
-            cartItemsContainer.appendChild(li); // Añadir al contenedor del carrito
+            cartItemsContainer.appendChild(li);
         });
     }
 
-    // Botones para finalizar compra y ver bolsa
+    // Bttns
+    cartItemsContainer.appendChild(document.createElement('hr'));
     const finalizaCompraButton = document.createElement('a');
     finalizaCompraButton.href = '#';
     finalizaCompraButton.classList.add('btn', 'btn-outline-secondary');
@@ -68,45 +63,54 @@ function updateCartDisplay() {
     verBolsaButton.classList.add('btn', 'btn-dark');
     verBolsaButton.innerText = 'Ver bolsa de compras';
 
-    cartItemsContainer.appendChild(finalizaCompraButton); // Añadir botón de finalizar compra
-    cartItemsContainer.appendChild(document.createElement('p')); // Espacio
-    cartItemsContainer.appendChild(verBolsaButton); // Añadir botón de ver bolsa
+    cartItemsContainer.appendChild(finalizaCompraButton);
+    cartItemsContainer.appendChild(document.createElement('p'));
+    cartItemsContainer.appendChild(verBolsaButton);
 }
 
-// Función para cargar los productos desde varios JSON
 function loadProducts() {
-    // Realizar múltiples fetch a los archivos JSON
+    // JSON's
     Promise.all([
         fetch('../JSON/callOfTheNight.json').then(response => response.json()),
-        fetch('../JSON/chainsawMan.json').then(response => response.json()), // Segundo archivo JSON
-        fetch('../JSON/kaguyaSama.json').then(response => response.json()) // Tercer archivo JSON
+        fetch('../JSON/chainsawMan.json').then(response => response.json()),
+        fetch('../JSON/kaguyaSama.json').then(response => response.json()),
+        fetch('../JSON/kamikatsu.json').then(response => response.json()),
+        fetch('../JSON/kimetsuNoYaiba.json').then(response => response.json()),
+        fetch('../JSON/kobayashisDragonMaid.json').then(response => response.json()),
+        fetch('../JSON/persona3.json').then(response => response.json()),
+        fetch('../JSON/persona5.json').then(response => response.json()),
+        fetch('../JSON/persona5Royal.json').then(response => response.json()),
+        fetch('../JSON/tomoChanIsAGirl.json').then(response => response.json())
     ])
     .then(data => {
-        // Combine todos los productos de los JSON en un solo array
         const allProducts = [
-            ...data[0].callOfTheNightProducts,  // productos del primer archivo JSON
-            ...data[1].chainsawManProducts,         // productos del segundo archivo JSON
-            ...data[2].kaguyaSamaProducts             // productos del tercer archivo JSON
+            ...data[0].callOfTheNightProducts,
+            ...data[1].chainsawManProducts,
+            ...data[2].kaguyaSamaProducts,
+            ...data[3].kamikatsuProducts,
+            ...data[4].kimetsuNoYaibaProducts,
+            ...data[5].kobayashisDragonMaidProducts,
+            ...data[6].persona3Products,
+            ...data[7].persona5Products,
+            ...data[8].persona5RoyalProducts,
+            ...data[9].tomoChanIsAGirlProducts
         ];
 
-        // Añadir eventos a los botones de "Agregar a la bolsa"
         const addToCartButtons = document.querySelectorAll('.btn-dark');
         addToCartButtons.forEach(button => {
-            const productName = button.getAttribute('data-product-name'); // Obtener el nombre del producto desde el atributo data
-            const product = allProducts.find(item => item.Name === productName); // Buscar el producto por nombre
+            const productName = button.getAttribute('data-product-name');
+            const product = allProducts.find(item => item.Name === productName);
 
-            // Asignar el evento de click para agregar el producto correcto al carrito
             button.addEventListener('click', function() {
-                addToCart(product); // Agregar producto al carrito
+                addToCart(product);
             });
         });
     })
     .catch(error => {
-        console.error('Error al cargar los JSON:', error); // Manejar errores
+        console.error('Error al cargar los JSON:', error);
     });
 }
 
-// Esperar a que el DOM esté completamente cargado
 document.addEventListener('DOMContentLoaded', function() {
-    loadProducts(); // Llamar a la función para cargar los productos cuando la página se cargue
+    loadProducts();
 });
